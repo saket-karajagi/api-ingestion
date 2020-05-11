73bc5b60c185b5712cc4e81e6c56cfcc100cd0b1
Saket Karajagi <saket@Sakets-MacBook-Pro.local>
2020-05-11 17:52:56 -0400

    adding ingestion process + view

---
+++ api_ingestion/ingest_api_to_rds.py
@@ -0,0 +1,99 @@
+import argparse
+import csv
+import psycopg2
+import pandas as pd
+from sodapy import Socrata
+from config import molekule_db, api_config
+from datetime import datetime
+
+def extract_data(url, key, dataset_id):
+    date = datetime.today().strftime('%Y-%m-%d')
+    client = Socrata(f"{url}", f"{key}")
+    print("extracting data from NYC Open Data\n")
+    results = client.get_all(f"{dataset_id}", content_type="csv")
+
+    row_count = 0 
+    with open(f'restaurants_{date}.csv', 'w') as f:
+        writer = csv.writer(f)
+        for item in results:
+            if row_count != 0 and item[0] != 'camis':
+                writer.writerow(item)
+            elif row_count == 0 and item[0] == 'camis':
+                writer.writerow(item)
+            row_count += 1
+    return f'restaurants_{date}.csv'
+
+def create_staging_table(cursor, field_names, table_name):
+    cursor.execute(f"""DROP TABLE IF EXISTS {table_name}_staging;""")
+
+    field_builder = ""
+    for i in range(0, len(field_names) - 1):
+        field = field_names[i]
+        field_builder += f'"{field}" varchar, '
+    last_field = field_names[-1]
+    field_builder += f'"{last_field}" varchar'
+
+    print("creating staging table\n")
+    create_staging_table_sql = f"""CREATE TABLE {table_name}_staging({field_builder});"""
+    print(create_staging_table_sql + '\n')
+    cursor.execute(create_staging_table_sql)
+
+def load_staging_table(cursor, file_name, table_name):
+    load_staging_table_sql = f"""COPY {table_name}_staging from STDIN CSV HEADER;"""
+    with open(file_name, 'r') as f:
+        print("loading staging table\n")
+        cursor.copy_expert(load_staging_table_sql, f)
+        print(load_staging_table_sql + '\n')
+
+def load_destination_table(cursor, field_names, table_name):
+    trunc_destination_table_sql = f"""TRUNCATE TABLE {table_name};"""
+    cursor.execute(trunc_destination_table_sql)
+    print(trunc_destination_table_sql + '\n')
+
+    field_builder = ""
+    for i in range(0, len(field_names) - 1):
+        field = field_names[i]
+        field_builder += f"{field}, "
+    last_field = field_names[-1]
+    field_builder += f"{last_field}"
+
+    load_destination_table_sql = f"""INSERT INTO {table_name}(data_blob)\
+    SELECT\
+    row_to_json((SELECT d FROM (SELECT {field_builder}) d)) AS data\
+    FROM {table_name}_staging;"""
+    print("loading destination table")
+    cursor.execute(load_destination_table_sql)
+    print(load_destination_table_sql + '\n')
+
+def main():
+    parser = argparse.ArgumentParser(description='Obtain dataset name')
+    parser.add_argument('-d', '--dataset_name', required=True, help='')
+    args = vars(parser.parse_args())
+
+    dataset_to_ingest = args['dataset_name']
+    dataset_config = api_config[dataset_to_ingest]
+    url, key, dataset_id = dataset_config['url'], dataset_config['app_key'], dataset_config['dataset_id']
+
+    ##connector to Amazon RDS where data is ingested
+    connection = psycopg2.connect(user = molekule_db['user'],
+      password = molekule_db['password'],
+      host = molekule_db['host'],
+      port = molekule_db['port'],
+      database = molekule_db['database'])
+
+    csv_file = extract_data(url, key, dataset_id)
+    
+    cursor = connection.cursor()
+
+    with open(csv_file, "r") as f:
+        reader = csv.reader(f)
+        field_names = next(reader)
+
+    create_staging_table(cursor, field_names, dataset_to_ingest)
+    load_staging_table(cursor, csv_file, dataset_to_ingest)
+    load_destination_table(cursor, field_names, dataset_to_ingest)
+
+    connection.commit()
+
+if __name__ == '__main__':
+    main()
---
+++ api_ingestion/views/v_restaurants.sql
@@ -0,0 +1,28 @@
+SELECT
+CAST(data_blob->>'camis' as integer) as camis
+,CAST(data_blob->>'dba' as varchar) as dba
+,CAST(data_blob->>'boro' as varchar) as boro
+,CAST(data_blob->>'building' as varchar) as building
+,CAST(data_blob->>'street' as varchar) as street
+,CAST(data_blob->>'zipcode' as varchar) as zipcode
+,CAST(data_blob->>'phone' as varchar) as phone
+,CAST(data_blob->>'cuisine_description' as varchar) as cuisine
+,CAST(data_blob->>'inspection_date' as timestamp) as inspection_date
+,CAST(data_blob->>'action' as varchar) as action
+,CAST(data_blob->>'violation_code' as varchar) as violation_code 
+,CAST(data_blob->>'violation_description' as varchar) as violation_description
+,CAST(data_blob->>'critical_flag' as varchar) as critical_flag
+,CAST(data_blob->>'score' as integer) as critical_flag
+,CAST(data_blob->>'grade' as varchar) as grade
+,CAST(data_blob->>'grade_date' as timestamp) as grade_date
+,CAST(data_blob->>'record_date' as timestamp) as record_date
+,CAST(data_blob->>'inspection_type' as varchar) as inspection_type
+,CAST(data_blob->>'latitude' as double precision) as latitude
+,CAST(data_blob->>'longitude' as double precision) as longitude
+,CAST(data_blob->>'community_board' as integer) as community_board
+,CAST(data_blob->>'council_district' as integer) as council_district
+,CAST(data_blob->>'census_tract' as integer) as census_tract
+,CAST(data_blob->>'bin' as integer) as bin
+,CAST(data_blob->>'bbl' as varchar) as bbl
+,CAST(data_blob->>'nta' as varchar) as nta
+FROM public.restaurants;
