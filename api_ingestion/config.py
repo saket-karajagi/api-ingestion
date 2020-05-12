5a4b80bc4fd44e75b66389661c8e51f518bdda7b
Saket Karajagi <saket@Sakets-MacBook-Pro.local>
2020-05-11 23:38:53 -0400

    adding config

---
+++ api_ingestion/config.py
@@ -0,0 +1,13 @@
+molekule_db = {'user':'etl_admin',
+            'password':'******',
+            'host': 'molekule.cdvvqsjgsryt.us-west-2.rds.amazonaws.com',
+            'port':'5432',
+            'database':'postgres'
+}
+
+api_config = {'restaurant_inspections':
+                   {'url': 'data.cityofnewyork.us',
+                    'app_key': '******',
+                    'dataset_id': '43nn-pn8j'
+                   }
+              }
