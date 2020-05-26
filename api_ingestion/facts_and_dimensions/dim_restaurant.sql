cff699798bc8125f6827adbda18d7dca3c7d2bdd
saket-karajagi <52709963+saket-karajagi@users.noreply.github.com>
2020-05-26 12:56:10 -0400

    Update dim_restaurant.sql

--- api_ingestion/facts_and_dimensions/dim_restaurant.sql
+++ api_ingestion/facts_and_dimensions/dim_restaurant.sql
@@ -9,5 +9,10 @@ DISTINCT
 camis,
 dba
 FROM public.restaurant_inspections
-WHERE NOT EXISTS (SELECT * FROM public.dim_restaurant r1
-                  WHERE r1.restaurant_id = camis);
+--WHERE NOT EXISTS (SELECT * FROM public.dim_restaurant r1
+--                  WHERE r1.restaurant_id = camis)
+WHERE record_date between current_date and date_sub(current_date, interval -7 day)
+ON CONFLICT (camis) 
+DO
+		UPDATE
+	  SET dba = EXCLUDED.dba;
